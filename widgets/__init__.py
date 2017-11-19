81c60285bf4097df5a000ce6bb79ec13f8c7bdb8
Caagr98 <caagr98@gmail.com>
2017-11-19 02:26:56 +0100

    Be more lenient with deps

--- widgets/__init__.py
+++ widgets/__init__.py
@@ -1,11 +1,22 @@
-from .separator import *
-from .clock import *
-from .battery import *
-from .temperature import *
-from .wifi import *
-from .ram import *
-from .cpugraph import *
-from .volume import *
-from .feed import *
-from .ibus import *
-from .mpd import *
+try: from .separator import *
+except ImportError: pass
+try: from .clock import *
+except ImportError: pass
+try: from .battery import *
+except ImportError: pass
+try: from .temperature import *
+except ImportError: pass
+try: from .wifi import *
+except ImportError: pass
+try: from .ram import *
+except ImportError: pass
+try: from .cpugraph import *
+except ImportError: pass
+try: from .volume import *
+except ImportError: pass
+try: from .feed import *
+except ImportError: pass
+try: from .ibus import *
+except ImportError: pass
+try: from .mpd import *
+except ImportError: pass
