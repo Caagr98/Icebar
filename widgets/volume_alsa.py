f61c630b95f0cafeebbdc4634b3b7f333349dad0
Caagr98 <caagr98@gmail.com>
2018-03-28 13:05:54 +0200

    Add a proper class for selems

--- widgets/volume_alsa.py
+++ widgets/volume_alsa.py
@@ -1,4 +1,4 @@
-from gi.repository import Gtk, Gdk, GLib, Keybinder
+from gi.repository import Gtk, GLib, Keybinder
 import ctypes as c
 import struct
 
@@ -15,43 +15,69 @@ def fn(f, ret, *args):
 	return f
 asound = c.CDLL("libasound.so")
 
-mixp = c.c_void_p
-open = fn(asound.snd_mixer_open, errno, P(mixp), c.c_int)
-attach = fn(asound.snd_mixer_attach, errno, mixp, c.c_char_p)
-selem_register = fn(asound.snd_mixer_selem_register, errno, mixp, c.c_void_p, P(c.c_void_p))
-load = fn(asound.snd_mixer_load, errno, mixp)
+class _alsa_array:
+	__slots__ = ["__selem", "__get", "__set", "__setall", "__type"]
+	def __init__(self, selem, get, set, setall, type):
+		self.__selem = selem
+		self.__get = get
+		self.__set = set
+		self.__setall = setall
+		self.__type = type
+	def __setitem__(self, k, v):
+		return self.__set(self.__selem, k, v)
+	def __getitem__(self, k):
+		p = self.__type()
+		self.__get(self.__selem, k, p)
+		return p.value
+	def __iter__(self):
+		for a in range(32):
+			if a in self.__selem:
+				yield self[a]
+	@property
+	def all(self):
+		return max(self)
+	@all.setter
+	def all(self, v):
+		self.__setall(self.__selem, v)
 
-selemidp = c.c_void_p
-selemp = c.c_void_p
-def selem_id_alloca(): return c.create_string_buffer(64)
-selem_id_set_index = fn(asound.snd_mixer_selem_id_set_index, None, selemidp, c.c_uint)
-selem_id_set_name = fn(asound.snd_mixer_selem_id_set_name, None, selemidp, c.c_char_p)
-find_selem = fn(asound.snd_mixer_find_selem, selemp, mixp, selemidp)
+class selem(c.c_void_p):
+	@property
+	def mB(self): # The ALSA functions are named dB, but they actually use millibel.
+		return _alsa_array(self, selem_get_playback_dB, selem_set_playback_dB, selem_set_playback_dB_all, c.c_long)
+	@property
+	def switch(self):
+		return _alsa_array(self, selem_get_playback_switch, selem_set_playback_switch, selem_set_playback_switch_all, c.c_bool)
+
+	@property
+	def mB_range(self):
+		p1, p2 = c.c_long(), c.c_long()
+		selem_get_playback_dB_range(self, p1, p2)
+		return int(p1.value), int(p2.value)
+
+	def __contains__(self, k):
+		return selem_has_playback_channel(self, k)
 
 chanid = c.c_int
-selem_ask_playback_vol_dB        = fn(asound.snd_mixer_selem_ask_playback_vol_dB,        errno, selemp, c.c_long, P(c.c_long))
-selem_has_playback_channel       = fn(asound.snd_mixer_selem_has_playback_channel,       c.c_bool, selemp, chanid)
 
-selem_has_common_volume          = fn(asound.snd_mixer_selem_has_common_volume,          c.c_bool, selemp)
-selem_has_playback_volume        = fn(asound.snd_mixer_selem_has_playback_volume,        c.c_bool, selemp)
-selem_has_playback_volume_joined = fn(asound.snd_mixer_selem_has_playback_volume_joined, c.c_bool, selemp)
-selem_get_playback_volume        = fn(asound.snd_mixer_selem_get_playback_volume,        errno, selemp, chanid, P(c.c_long))
-selem_set_playback_volume        = fn(asound.snd_mixer_selem_set_playback_volume,        errno, selemp, chanid, c.c_long)
-selem_set_playback_volume_all    = fn(asound.snd_mixer_selem_set_playback_volume_all,    errno, selemp, c.c_long)
-selem_get_playback_dB            = fn(asound.snd_mixer_selem_get_playback_dB,            errno, selemp, chanid, P(c.c_long))
-selem_set_playback_dB            = fn(asound.snd_mixer_selem_set_playback_dB,            errno, selemp, chanid, c.c_long)
-selem_set_playback_dB_all        = fn(asound.snd_mixer_selem_set_playback_dB_all,        errno, selemp, c.c_long)
+mixp = c.c_void_p
+open            = fn(asound.snd_mixer_open,            errno, P(mixp), c.c_int)
+attach          = fn(asound.snd_mixer_attach,          errno, mixp, c.c_char_p)
+selem_register  = fn(asound.snd_mixer_selem_register,  errno, mixp, c.c_void_p, P(c.c_void_p))
+load            = fn(asound.snd_mixer_load,            errno, mixp)
 
-selem_has_common_switch          = fn(asound.snd_mixer_selem_has_common_volume,          c.c_bool, selemp)
-selem_has_playback_switch        = fn(asound.snd_mixer_selem_has_playback_volume,        c.c_bool, selemp)
-selem_has_playback_switch_joined = fn(asound.snd_mixer_selem_has_playback_volume_joined, c.c_bool, selemp)
-selem_get_playback_switch        = fn(asound.snd_mixer_selem_get_playback_switch,        errno, selemp, chanid, P(c.c_bool))
-selem_set_playback_switch        = fn(asound.snd_mixer_selem_set_playback_switch,        errno, selemp, chanid, c.c_bool)
-selem_set_playback_switch_all    = fn(asound.snd_mixer_selem_set_playback_switch_all,    errno, selemp, c.c_bool)
+find_selem                       = fn(asound.snd_mixer_find_selem,                       selem, mixp, c.c_void_p)
 
-selem_get_playback_volume_range  = fn(asound.snd_mixer_selem_get_playback_volume_range,  errno, selemp, P(c.c_long), P(c.c_long))
-selem_set_playback_volume_range  = fn(asound.snd_mixer_selem_set_playback_volume_range,  errno, selemp, c.c_long, c.c_long)
-selem_get_playback_dB_range      = fn(asound.snd_mixer_selem_get_playback_dB_range,      errno, selemp, P(c.c_long), P(c.c_long))
+selem_has_playback_channel       = fn(asound.snd_mixer_selem_has_playback_channel,       c.c_bool, selem, chanid)
+
+selem_get_playback_dB            = fn(asound.snd_mixer_selem_get_playback_dB,            errno, selem, chanid, P(c.c_long))
+selem_set_playback_dB            = fn(asound.snd_mixer_selem_set_playback_dB,            errno, selem, chanid, c.c_long)
+selem_set_playback_dB_all        = fn(asound.snd_mixer_selem_set_playback_dB_all,        errno, selem, c.c_long)
+
+selem_get_playback_switch        = fn(asound.snd_mixer_selem_get_playback_switch,        errno, selem, chanid, P(c.c_bool))
+selem_set_playback_switch        = fn(asound.snd_mixer_selem_set_playback_switch,        errno, selem, chanid, c.c_bool)
+selem_set_playback_switch_all    = fn(asound.snd_mixer_selem_set_playback_switch_all,    errno, selem, c.c_bool)
+
+selem_get_playback_dB_range      = fn(asound.snd_mixer_selem_get_playback_dB_range,      errno, selem, P(c.c_long), P(c.c_long))
 
 class pollfd(c.Structure):
 	_fields_ = [
@@ -63,7 +89,7 @@ poll_descriptors = fn(asound.snd_mixer_poll_descriptors, c.c_int, mixp, P(pollfd
 poll_descriptors_count = fn(asound.snd_mixer_poll_descriptors_count, c.c_int, mixp)
 
 elem_callback = c.CFUNCTYPE(c.c_int, mixp, c.c_uint)
-elem_set_callback = fn(asound.snd_mixer_elem_set_callback, None, selemp, elem_callback)
+elem_set_callback = fn(asound.snd_mixer_elem_set_callback, None, selem, elem_callback)
 handle_events = fn(asound.snd_mixer_handle_events, mixp)
 
 
@@ -92,9 +118,7 @@ class AlsaVolume(Gtk.EventBox):
 		load(self.handle)
 		self.elem = find_selem(self.handle, struct.pack("<60sI", name.encode(), id))
 
-		minp, maxp = c.c_long(), c.c_long()
-		selem_get_playback_dB_range(self.elem, minp, maxp)
-		self.min, self.max = minp.value, maxp.value
+		self.min, self.max = self.elem.mB_range
 
 		self.update()
 		self.ggg = elem_callback(self.update)
@@ -107,57 +131,18 @@ class AlsaVolume(Gtk.EventBox):
 			GLib.io_add_watch(a.fd, GLib.IO_IN, lambda *_: handle_events(self.handle) or self.update() or True)
 
 		if keys:
-			Keybinder.bind("AudioMute", lambda _: self.setMute(not self.getMute()))
-			Keybinder.bind("AudioRaiseVolume", lambda _: self.changeVolume(+2))
-			Keybinder.bind("AudioLowerVolume", lambda _: self.changeVolume(-2))
+			def toggleSwitch(): self.elem.switch.all = not self.elem.switch.all
+			def setSwitch(v): self.elem.switch.all = v
+			def changeVolume(n): self.elem.mB.all += n
+			Keybinder.bind("AudioMute", lambda _: toggleSwitch())
+			Keybinder.bind("AudioRaiseVolume", lambda _: changeVolume(+250))
+			Keybinder.bind("AudioLowerVolume", lambda _: changeVolume(-250))
+			Keybinder.bind("<Shift>AudioMute", lambda _: setSwitch(False))
+			Keybinder.bind("<Shift>AudioRaiseVolume", lambda _: changeVolume(+50))
+			Keybinder.bind("<Shift>AudioLowerVolume", lambda _: changeVolume(-50))
 
 	def update(self, *_):
-		self.updateVolume()
-		self.updateMute()
-		return False
-
-	def updateVolume(self):
-		self.text.set_text("{:.1f} dB".format(self.getVolume_() / 100 + self.base))
+		self.text.set_text("{:.1f} dB".format(self.elem.mB.all / 100 + self.base))
 		self.icon.set_text("♪")
-
-	def updateMute(self):
-		self.set_opacity(0.5 if self.getMute() else 1)
-
-	def getMute(self):
-		a = []
-		p = c.c_bool()
-		for i in range(32):
-			if selem_has_playback_channel(self.elem, i):
-				selem_get_playback_switch(self.elem, i, p)
-				a.append(p.value)
-		return not all(a)
-
-	def setMute(self, m):
-		selem_set_playback_switch_all(self.elem, not m)
-
-	def getVolume(self):
-		return self.norm(self.getVolume_())
-	def getVolume_(self):
-		a = []
-		p = c.c_long()
-		for i in range(32):
-			if selem_has_playback_channel(self.elem, i):
-				selem_get_playback_dB(self.elem, i, p)
-				a.append(p.value)
-		return max(a)
-
-	def setVolume(self, v):
-		return self.setVolume_(self.denorm(v))
-	def setVolume_(self, v):
-		selem_set_playback_dB_all(self.elem, v)
-
-	def toDB(self, v):
-		return self.denorm(v) / 100
-
-	def norm(self, v):
-		return (v-self.min)/(self.max-self.min)
-	def denorm(self, v):
-		return round(v*(self.max-self.min)+self.min)
-
-	def changeVolume(self, d):
-		self.setVolume_(self.getVolume_() + d * 250)
+		self.set_opacity(1 if self.elem.switch.all else 0.5)
+		return False
